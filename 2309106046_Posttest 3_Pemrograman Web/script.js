d2330fd313195fb85aeef053bdb12da017dfc908
arif12904 <149857196+arif12904@users.noreply.github.com>
2024-09-23 04:27:27 +0800

    Add files via upload

---
+++ 2309106046_Posttest 3_Pemrograman Web/script.js
@@ -0,0 +1,27 @@
+let sideBar = document.getElementById("sideBar")
+let root = document.documentElement
+let button = document.getElementById("toggleMode")
+let icon = document.getElementById("toggleicon")
+
+document.getElementById("hamburgerMenu").addEventListener("click", function(event){
+    event.stopPropagation()
+    sideBar.classList.add("active")
+})
+ 
+
+window.addEventListener("click", function(){
+    if(sideBar.classList.contains("active")){
+        sideBar.classList.remove("active")
+    }
+})
+
+button.addEventListener("click", function(){
+    root.classList.toggle("dark")
+    if(root.classList.contains("dark")){
+        icon.src = "asset/sun.png"
+        icon.alt = "sun"
+    } else{
+        icon.src = "asset/moon.png"
+        icon.alt = "moon"
+    }
+})
